80da61e3a53090e31708e318b0aeb32514806ff9
inmobiliariasobrelaroca-prog <inmobiliariasobrelaroca@gmail.com>
2026-09-02 20:13:34 -0600

    Update version from v32 to v33

--- src/ModuloTesoreria.tsx
+++ src/ModuloTesoreria.tsx
@@ -33,7 +33,7 @@ import Anticipos from "./tesoreria/Anticipos";
 
 // Se muestra en el encabezado del módulo. Sirve para saber de un
 // vistazo qué versión quedó desplegada, sin abrir el repositorio.
-const VERSION = "v32";
+const VERSION = "v33";
 
 // Mismo patrón de eventos de ventana que ya usa el aviso de código
 // pendiente. Permite poner el botón en el TopBar sin tener que pasar
